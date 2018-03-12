b64a60795c0dc6b3ccd0a5720edbb5ac636c05a8
emiliska <33560517+emiliska@users.noreply.github.com>
2018-03-11 23:08:16 -0400

    Initial commit

---
+++ install_startup.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# install Flask
+sudo apt-get install python-pip
+pip install --user Flask
+
+# install Apache, WSGI(Web Server Gateway Interface)
+sudo apt-get install apache2 -y
+sudo apt-get install libapache2-mod-wsgi -y
+
