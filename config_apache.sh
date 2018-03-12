c114e012975b20c5f7e3f1e55d2915515b40eb60
emiliska <33560517+emiliska@users.noreply.github.com>
2018-03-12 13:03:28 -0400

    Update config_apache.sh

--- config_apache.sh
+++ config_apache.sh
@@ -11,23 +11,8 @@ $USERNAME = username
 sudo chown -R $USERNAME /var/www
 sudo chown -R $USERNAME /etc/apache2
 
-# create wsgi file in /var/www
-sudo touch /var/www/hello.wsgi
-echo 'import sys' >> /var/www/hello.wsgi
-echo 'sys.path.insert(0,"/var/www/firstapp")' >> /var/www/hello.wsgi
-echo 'from hello imkport app as application' >> /var/www/hello.wsgi
+# copy files from firstApp folder to appropriate location 
+#	within Ubuntu
+sudo cp /firstapp/hello.conf /etc/apache2/hello.conf
+sudo cp /firstapp/hello.wsgi /var/www
 
-# create apache config file
-sudo touch /etc/apache2/hello.conf
-echo '<VirtualHost*>' >> /etc/apache2/hello.conf
-echo 'ServerName example.com' >> /etc/apache2/hello.conf
-echo '' >> /etc/apache2/hello.conf
-echo 'WSGIScriptAlias //var/www/firstapp/hello.wsgi WSGIDaemonProcess hello' >> /etc/apache2/hello.conf
-'<Directory /var/www/firstapp>' >> /etc/apache2/hello.conf
-echo '' >> /etc/apache2/hello.conf
-echo 'WSGIProcessGroup hello' >> /etc/apache2/hello.conf
-echo 'WSGIApplicationGroup %{GLOBAL}' >> /etc/apache2/hello.conf
-echo 'Order deny,allow' >> /etc/apache2/hello.conf
-echo 'Allow from all' >> /etc/apache2/hello.conf
-echo '</Directory>' >> /etc/apache2/hello.conf
-echo '</VirtualHost>' >> /etc/apache2/hello.conf
