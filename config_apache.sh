c979f22086453a0289334dc5b829d68cf7ecca05
emiliska <33560517+emiliska@users.noreply.github.com>
2018-03-12 01:28:17 -0400

    Update config_apache.sh

--- config_apache.sh
+++ config_apache.sh
@@ -7,8 +7,3 @@
 # give your user privs, replace username with your username
 sudo chown -R $USERNAME /var/www
 
-# git clone this repo
-git clone https://github.com/emiliska/gcloud-Ubuntu-14-and-Flask-setup.git
-
-# give scripts permission to execute
-sudo chmod -x /gcloud-Ubuntu-14-and-Flask-setup/*.sh
